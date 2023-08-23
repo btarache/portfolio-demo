1f59a3c46bf5a1b05412933a3538a49d4b00ece3
btarache <benjamintarache@gmail.com>
2023-08-22 21:40:40 -0500

    Modif sanity.client a FALSE

--- sanity/sanity.client.ts
+++ sanity/sanity.client.ts
@@ -6,7 +6,7 @@ const config: ClientConfig = {
   projectId: "8qiyfvhk", /* <---------        Este es el del DEMO: ga8lllhf */
   dataset: "production",
   apiVersion: "2023-07-16",
-  useCdn: true,
+  useCdn: false,
 };
 
 const client = createClient(config);
